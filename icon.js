67011ae5ddd55a7c71ced0fefa4c7f3968b99a93
Muthukarthick T <98314834+Muka36@users.noreply.github.com>
2022-04-10 23:45:06 +0530

    Add files via upload

---
+++ icon.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {FaTimes, FaPen, FaRegCircle} from 'react-icons/fa';
+
+
+const Icon = ({sample}) => {
+    return (
+        <h1><FaTimes className = "icon"/>
+        Hello</h1>
+    )
+}
+
+export default Icon;
